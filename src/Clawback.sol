cd207a950b87aa82e5de78944b5904b51734e3c5
flyjgh <casselescouilles19@gmail.com>
2024-09-28 23:15:09 +0100

    update clawback

--- src/Clawback.sol
+++ src/Clawback.sol
@@ -7,28 +7,35 @@ contract Clawback {
     /// @notice ERC20-claimee inclusion root
     bytes32 public immutable MERKLE_ROOT;
 
+    address public immutable ROSE;
+
+    uint constant BASE_AMOUNT = 100000e18;
+
+    bytes32 constant TRANSFER_SELECTOR = keccak256("transfer(address,uint256)");
+
     /// @notice Mapping of addresses who have claimed tokens
     mapping(address => bool) public hasClaimed;
 
-    constructor(bytes32 root) {
+    constructor(bytes32 root, address rose) {
         MERKLE_ROOT = root;
+        ROSE = rose;
     }
 
     /// @notice Allows claiming tokens if address is part of merkle tree
-    /// @param to address of claimee
-    /// @param amount of tokens owed to claimee
-    /// @param proof merkle proof to prove address and amount are in tree
-    function claim(address to, uint256 amount, bytes32[] calldata proof) external {
+    /// @param account address of claimee
+    /// @param proof merkle proof to prove address is in tree
+    function claim(address account, bytes32[] calldata proof) external {
         // Throw if address has already claimed tokens
-        if (hasClaimed[to]) revert AlreadyClaimed();
+        if (hasClaimed[account]) revert AlreadyClaimed();
         // Verify merkle proof, or revert if not in tree
-        bytes32 leaf = keccak256(abi.encodePacked(to, amount));
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account))));
         bool isValidLeaf = MerkleProof.verify(proof, MERKLE_ROOT, leaf);
         if (!isValidLeaf) revert NotInMerkle();
 
-        hasClaimed[to] = true;
+        hasClaimed[account] = true;
 
-        _mint(to, amount);
-        emit Claim(to, amount);
+        (bool success, bytes memory data) = ROSE.call(abi.encodeWithSelector(TRANSFER_SELECTOR, account, BASE_AMOUNT));
+        if (!success || data.length == 0) revert TransferFailed();
+        emit Claim(account, BASE_AMOUNT);
     }
 }
